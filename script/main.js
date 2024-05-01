dc063b56c741e0810a064ca188f492cbeca02c03
Tseren Mandzhiev <fmtseri4@gmail.com>
2024-05-01 14:03:54 +0300

    что успел как говорится

---
+++ script/main.js
@@ -0,0 +1,22 @@
+const burger = document.querySelector('.nav__burger');
+
+const burgerLine = document.querySelector('.burger-line');
+
+const headerNav = document.querySelector('.header__menu-nav');
+
+
+burger.addEventListener('click', function() {
+	burgerLine.classList.toggle('activeBtn');
+	headerNav.classList.toggle('nav__active');
+})
+
+const formBtn = document.querySelector('.modalWindow');
+
+const modal = document.querySelector('.modalActive');
+
+formBtn.addEventListener('click', function() {
+	modal.classList.toggle('open');
+	
+});
+
+
